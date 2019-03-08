868f224f61b48a5a15d491e5a8b5243bc0ca21d0
Ankur Anant <ankuranant99@gmail.com>
2019-03-09 01:33:10 +0530

    minor change

--- Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/monitor/ChartView.java
+++ Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/monitor/ChartView.java
@@ -1,15 +1,18 @@
 package com.sonelli.juicessh.performancemonitor.monitor;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.sonelli.juicessh.performancemonitor.R;
+import com.sonelli.juicessh.performancemonitor.helpers.PreferenceHelper;
 
 public class ChartView extends AppCompatActivity {
 
@@ -38,18 +41,28 @@ public class ChartView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_view);
 
+        PreferenceHelper preferenceHelper = new PreferenceHelper(this);
+        Log.d("chartview", "print: "+preferenceHelper.getKeepScreenOnFlag());
+        if(preferenceHelper.getKeepScreenOnFlag()){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
         GraphView graph = findViewById(R.id.graph);
 
         Viewport viewport = graph.getViewport();
         viewport.setXAxisBoundsManual(true);
         viewport.setMinX(0);
-        viewport.setMinY(0);
         viewport.setMaxX(20);
         viewport.setScrollable(true);
-        viewport.isScalable();
+        viewport.setScalable(true);
+
 
         series.setDrawDataPoints(true);
         series.setDrawBackground(true);
+        series.setAnimated(true);
+        series.setDrawAsPath(true);
+        series.setColor(Color.rgb(156, 39, 176));
+        series.setBackgroundColor(Color.argb(40, 76, 175, 80));
         graph.addSeries(series);
     }
 
@@ -59,7 +72,6 @@ public class ChartView extends AppCompatActivity {
         mTimer = new Runnable() {
             @Override
             public void run() {
-                Log.d("ffffffffffffffff", "run " + cpuStat);
                 addEntry(cpuStat);
                 mHandler.postDelayed(this, 1000);
             }
@@ -75,7 +87,6 @@ public class ChartView extends AppCompatActivity {
 
     public void updater(int cpu) {
         cpuStat = cpu;
-        Log.d("ccccppppppuuuu", String.valueOf(cpu));
         addEntry(cpu);
     }
 
